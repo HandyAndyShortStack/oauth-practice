803133af725fa44c5d316d675c5bd96ff7a7228b
Andy Short <andynshort@gmail.com>
2013-06-10 18:32:01 -0700

    added empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OauthPractice::Application.config.secret_token = '1a9ffbc96629da7d52a0bf0d00a9588ee2fa0dc80a84e1c898554fbdb140044585c15ddd9ef3bd00230b8047483ea716aa04be55dda143f404bcc477187da037'
